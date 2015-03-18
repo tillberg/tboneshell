5c707d368be001aa0317ebae6c5e3b07544576f6
Aaron Sikes <jingadjanga@gmail.com>
2015-03-18 13:13:11 -0400

    Generate some data with permute

---
+++ data.js
@@ -0,0 +1,722 @@
+T('data', [
+  {
+    "color": "red",
+    "size": "small",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "small",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "small",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "small",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "small",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "small",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "medium",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "medium",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "medium",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "medium",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "medium",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "medium",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "large",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "large",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "large",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "large",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "large",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "large",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "massive",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "massive",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "massive",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "red",
+    "size": "massive",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "red",
+    "size": "massive",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "red",
+    "size": "massive",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "small",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "small",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "small",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "small",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "small",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "small",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "medium",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "medium",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "medium",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "medium",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "medium",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "medium",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "large",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "large",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "large",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "large",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "large",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "large",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "massive",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "massive",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "massive",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "green",
+    "size": "massive",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "green",
+    "size": "massive",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "green",
+    "size": "massive",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "small",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "small",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "small",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "small",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "small",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "small",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "medium",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "medium",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "medium",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "medium",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "medium",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "medium",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "large",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "large",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "large",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "large",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "large",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "large",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "massive",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "massive",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "massive",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "blue",
+    "size": "massive",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "blue",
+    "size": "massive",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "blue",
+    "size": "massive",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "small",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "small",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "small",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "small",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "small",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "small",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "medium",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "medium",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "medium",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "medium",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "medium",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "medium",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "large",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "large",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "large",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "large",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "large",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "large",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "massive",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "massive",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "massive",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "black",
+    "size": "massive",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "black",
+    "size": "massive",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "black",
+    "size": "massive",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "small",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "small",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "small",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "small",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "small",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "small",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "medium",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "medium",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "medium",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "medium",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "medium",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "medium",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "large",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "large",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "large",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "large",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "large",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "large",
+    "visibility": "private",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "massive",
+    "visibility": "public",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "massive",
+    "visibility": "public",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "massive",
+    "visibility": "public",
+    "time": "future"
+  },
+  {
+    "color": "white",
+    "size": "massive",
+    "visibility": "private",
+    "time": "past"
+  },
+  {
+    "color": "white",
+    "size": "massive",
+    "visibility": "private",
+    "time": "present"
+  },
+  {
+    "color": "white",
+    "size": "massive",
+    "visibility": "private",
+    "time": "future"
+  }
+])
