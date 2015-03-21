47176b7b398a54f0bf15fc95a20ed1c8e945dbfa
Dan Tillberg <dan.tillberg@threatstack.com>
2015-03-21 10:12:18 -0400

    Move sort settings to T('mem') and T('defaultedSort')

--- js/SortOrder.js
+++ js/SortOrder.js
@@ -1,26 +1,9 @@
 var _ = require('lodash')
 
-function SortOrder(property, direction){
-	this.property = property
-	this.direction = direction
-}
-
-SortOrder.prototype.sortBy = function sortBy(property){
-	var direction = SortOrder.ASCENDING
-	if (property === this.property)
-		direction = !this.direction
-
-	return new SortOrder(property, direction)
-}
-
-SortOrder.prototype.sortThusly = function sortThusly(array){
-	var result = _.sortBy(array, this.property)
-	if (this.direction === SortOrder.DESCENDING)
+exports.sortThusly = function sortThusly(array, prop, isAscending){
+	var result = _.sortBy(array, prop)
+	if (!isAscending) {
 		result.reverse()
+	}
 	return result
 }
-
-SortOrder.ASCENDING = true
-SortOrder.DESCENDING = false
-
-module.exports = SortOrder
