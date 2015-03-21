dfcbe01be4c193e17e4fa579225613cc8bde6cfa
Dan Tillberg <dan.tillberg@threatstack.com>
2015-03-21 10:14:31 -0400

    Rebuild build.js

--- build.js
+++ build.js
@@ -19,30 +19,13 @@ module.exports = {
 },{"lodash":7}],2:[function(require,module,exports){
 var _ = require('lodash')
 
-function SortOrder(property, direction){
-	this.property = property
-	this.direction = direction
-}
-
-SortOrder.prototype.sortBy = function sortBy(property){
-	var direction = SortOrder.ASCENDING
-	if (property === this.property)
-		direction = !this.direction
-
-	return new SortOrder(property, direction)
-}
-
-SortOrder.prototype.sortThusly = function sortThusly(array){
-	var result = _.sortBy(array, this.property)
-	if (this.direction === SortOrder.DESCENDING)
+exports.sortThusly = function sortThusly(array, prop, isAscending){
+	var result = _.sortBy(array, prop)
+	if (!isAscending) {
 		result.reverse()
+	}
 	return result
 }
-
-SortOrder.ASCENDING = true
-SortOrder.DESCENDING = false
-
-module.exports = SortOrder
 
 },{"lodash":7}],3:[function(require,module,exports){
 function double(n){
@@ -784,18 +767,64 @@ module.exports = [
 var core = require('./core')
 var data = require('./data')
 
+// Overview:
+// - We *set* sort settings only in T('mem')
+// - We *read* sort settings only from T('currentSort')
+//
+// This gives us flexibility to define defaults however
+// we want, while making a very clear distinction between
+// defaults and values that were explicitly set by the user.
+//
+// Also, we are constraining all the page state to specific
+// buckets, making a clear distinction between data that
+// represents the changeable state of the page, and data
+// that has been computed from it. The goal is to have the
+// bulk of the code/computationse/etc take the form of
+// *computed* values, while reducing down the "changeable
+// state" of the page to its bare minimum.
+//
+// For example, the sortedData is not part of the *state* of
+// the page, and shouldn't be stored in the URL or
+// localStorage or anything like that. It can be *derived*
+// from the: data that we load from a specific URL on the
+// server, the current sort property, and the current sort
+// direction.
+
+// T('mem') is where I store all transient page state.
+// Everything else here should be some sort of `bound`
+// model (including e.g. ajax models, which in a real
+// application, T('data') would be an ajax model or
+// something along those lines).
+T('mem', {
+	sortProp: null,
+	sortAsc: null,
+});
+
+// T('currentSort') calculates the *defaulted* sort
+// property and direction.
+var defaultSortProp = 'size_on_disk';
+var defaultSortAsc = true;
+T('defaultedSort', function() {
+	var sortProp = T('mem.sortProp');
+	var sortAsc = T('mem.sortAsc');
+	return {
+		prop: sortProp != null ? sortProp : defaultSortProp,
+		asc: sortAsc != null ? sortAsc : defaultSortAsc,
+	};
+});
+
 T('data', data)
 T('decoratedData', function(){
 	var data = T('data')
 
 	return data.map(core.Decorator.decorate)
 })
-T('currentSort', new core.SortOrder())
 T('sortedData', function(){
-	var data = T('decoratedData')
-	var sort = T('currentSort')
+	var data = T('decoratedData');
+	var sortProp = T('defaultedSort.prop');
+	var sortAsc = T('defaultedSort.asc');
 
-	return sort.sortThusly(data)
+	return core.SortOrder.sortThusly(data, sortProp, sortAsc);
 })
 
 $(function(){
@@ -804,10 +833,14 @@ $(function(){
 		var html = core.templates.table(data)
 		$('table')[0].outerHTML = html
 		$('th').click(function(){
-			var sortProperty = $(this).text().toLowerCase()
-			var sort = T('currentSort')
-
-			T('currentSort', sort.sortBy(sortProperty))
+			var newSortProp = $(this).text().toLowerCase();
+			var currentSortProp = T.readSilent('defaultedSort.prop');
+			if (newSortProp !== currentSortProp) {
+				T('mem.sortProp', newSortProp);
+				T('mem.sortAsc', null);
+			} else {
+				T('mem.sortAsc', !T.readSilent('defaultedSort.asc'));
+			}
 		})
 	})
 })
